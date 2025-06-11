d2b11b626f4a8289edb6d8e789db09456f8ea083
sam <sam@weserve.tw>
2025-06-11 18:48:22 +0800

    fix: comment out basePath and assetPrefix in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
-const isDev = process.env.NODE_ENV === 'development';
-const basePath = isDev ? '' : '/wazy_web';
+// const isDev = process.env.NODE_ENV === 'development';
+// const basePath = isDev ? '' : '/wazy_web';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
   distDir: './dist',
-  basePath,
-  assetPrefix: basePath,
+  // basePath,
+  // assetPrefix: basePath,
 };
 
 export default nextConfig;

6d4d48554b3f9f8127e7a83180a38144b1d73ade
sam <sam@weserve.tw>
2025-06-11 22:36:46 +0800

    fix: replace Head and Script with GoogleTagManager in layout.tsx; update package.json and package-lock.json to include @next/third-parties

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import Layout from '@/layouts/index';
+import { GoogleTagManager } from '@next/third-parties/google';
 import { Metadata } from 'next';
-import Head from 'next/head';
-import Script from 'next/script';
 import './globals.css';
 
 export const metadata: Metadata = {
@@ -12,22 +11,9 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <Head>
-        <Script id="gtm-init" strategy="beforeInteractive">
-          {`(function (w, d, s, l, i) {
-            w[l] = w[l] || [];
-            w[l].push({ 'gtm.start': new Date().getTime(), event: 'gtm.js' });
-            const f = d.getElementsByTagName(s)[0],
-              j = d.createElement(s),
-              dl = l != 'dataLayer' ? '&l=' + l : '';
-            j.async = true;
-            j.src = 'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
-            f.parentNode.insertBefore(j, f);
-          })(window, document, 'script', 'dataLayer', 'GTM-MJ39V9Z6')`}
-        </Script>
-      </Head>
-      <body>
+    <html lang="en" suppressHydrationWarning>
+      <GoogleTagManager gtmId="GTM-MJ39V9Z6" />
+      <body suppressHydrationWarning>
         <div id="root">
           <Layout>{children}</Layout>
         </div>
